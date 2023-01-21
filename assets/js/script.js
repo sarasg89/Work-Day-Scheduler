5f6fec0c02c9121e601e395dbfdbfe35da438699
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-01-21 11:43:54 -0500

    created functiont to apply past, present and future classes to each hour block based on the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+// I wrap all my JS code inside the .ready method to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+$(document).ready(function () {
+  var todayEl = $("#currentDay");
+  var saveBtnEl = $(".saveBtn");
+  var mainEl = $("main");
+  var descriptionEl = $("textarea");
+
+
+  var now = dayjs().format("dddd") + " " + dayjs().format("MMMM D, YYYY");
+  var time = dayjs().format("HH:MM");
+  todayEl.text("Today is " + now + ". The time is " + time);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  saveBtnEl.on("click", function() {});
+
+
+  // This function applies the past, present, or future class to each time block by comparing the id to the current hour. 
+  
+  function applyTimeClass() {
+    var timeIdEl = $(".time-block");
+    var timeNow = dayjs().format("HH");
+    for (i = 0; i < timeIdEl.length; i++) {
+      hourFromId = timeIdEl[i].getAttribute("id").slice(-2)
+      if (hourFromId < timeNow) {
+        oldClass = descriptionEl[i].getAttribute("class");
+        newClass = oldClass + " past"
+        descriptionEl[i].setAttribute("class", newClass);
+      } else if (hourFromId === timeNow) {
+        oldClass = descriptionEl[i].getAttribute("class");
+        newClass = oldClass + " present"
+        descriptionEl[i].setAttribute("class", newClass);
+      } else {
+        oldClass = descriptionEl[i].getAttribute("class");
+        newClass = oldClass + " future"
+        descriptionEl[i].setAttribute("class", newClass);
+      }
+    };
+  };
+
+  applyTimeClass();
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -21,3 +53,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
